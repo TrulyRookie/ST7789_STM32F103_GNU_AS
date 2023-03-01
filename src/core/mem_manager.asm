d54bab88716fe0e4672e5c3664c8da3abe7a5bcf
TrulyRookie <b-allbreaker@yandex.ru>
2023-03-01 10:29:34 +0300

    Update mem_manager.asm
    Filled array correction

--- src/core/mem_manager.asm
+++ src/core/mem_manager.asm
@@ -15,7 +15,7 @@
      .align (2)
      TOTAL_FREE_RAM_MEMORY: .word 0x0
      FREE_RAM:.word 0X0
-     BLOCKS_ARRAY:.word 0x0
+     @BLOCKS_ARRAY:.word 0x0
 .section .asmcode
      .word FREE_SRAM_START
 
@@ -26,8 +26,8 @@ GetFreeMemAmount:    @out - free memory amount stores in TOTAL_FREE_RAM_MEMORY
      LDR r1, =FREE_RAM        @MOVD R1, FREE_RAM                  @POINTER TO FREE MEMORY           @r1 = {FREE_RAM* = null}
      STR R0, [R1]                       @ FREE_RAM = FREE_SRAM_START {FREE_RAM* = free_memory_address}
      MOV R1, SP                         @ r1 = STACK_TOP_ADDR
-     ADD R1, #100
-     RSB r0, R1                     @ r0 = STACK_TOP - free_memory_address (total free memory amout in bytes)
+     ADD R1, #200                       @ buffer for 50 stack writes
+     RSB r0, R1                         @ r0 = STACK_TOP - free_memory_address (total free memory amout in bytes)
      LDR r1, =TOTAL_FREE_RAM_MEMORY     @MOVD r1, TOTAL_FREE_RAM_MEMORY     @ r0 = TOTAL_FREE_RAM_MEMORY {TOTAL_FREE_RAM_MEMORY* = mem_size}
      STR r0, [r1]                       @ TOTAL_FREE_RAM_MEMORY* = new_mem_size
      POP {R0,r1, LR}
@@ -53,11 +53,11 @@ GetArray:
           MVN r5, #1
           ADD r5, #1                      @ r5 = 0xFFFFFFFF
           LSL R4, r0, r1                  @ byte_array_size{r4} = size * data_size
-          LSL R2, R1, #16
-          ORR R0, R0, R2
-          ADD r4, #4                      @ byte_array_size += 4
+          LSL R2, R1, #16                 @ data size in hi hword
+          ORR R0, R0, R2                  @ merge data size with array size
+          ADD r4, #4                      @ byte_array_size += 4 - word buffer for size storing
           BL GetFreeMemAmount             @ check valid free memory
-          LDR r1, =TOTAL_FREE_RAM_MEMORY  @r1 = pTOTAL_FREE_RAM_MEMORY
+          LDR r1, =TOTAL_FREE_RAM_MEMORY  @r1 = pTOTAL_FREE_RAM_MEMORY  - pointer to free menmory amount
           LDR r1, [r1]                    @r1 = *pTOTAL_FREE_RAM_MEMORY
           cmp r1, r4                      @ if (*TOTAL_FREE_RAM_MEMORY < array_size)
           bmi GetArray_exit               @ then return -1
@@ -74,7 +74,7 @@ GetArray_exit:                            @ return array{r5}
 @ IN: r0 - pointer to array
 ReleaseArray:                    @ ReleaseArray(*array)
      push {r0-r4,LR}
-          SUB r0, #4             @ array{r0} -= 4 -> *array{[r0]} == array_size
+          SUB r0, #4             @ array{r0} -= 4 -> *array{[r0]} == array_size - array start address
           LDR r2, =FREE_RAM      @ r2 = ppFREE_RAM
           str r0, [r2]           @ *ppFREE_RAM{[r2]} = r3
      pop {r0-r4,LR}              @ return
@@ -97,10 +97,10 @@ ReleaseArray:                    @ ReleaseArray(*array)
 .global GetFilledArray
 GetFilledArray:
      PUSH {r0-r4,LR}
-          BL GetArray
-          MOV r3, #0
+          BL GetArray         @ make array R0 size of R1 data size
+          MOV r3, #0          @ cycle counter
 GetFilledArray_Fill_Loop:
-          MUL r4, r3, r1
+          LSL r4, r3, r1
           CMP r1, #0
           BEQ GFA_BYTE
           CMP r1, #1
